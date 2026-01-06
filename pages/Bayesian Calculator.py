7e43dbe20e8cb87edbd893a069520d41753e564c
Stan Hamerling <stan.hamerling@persgroep.net>
2026-01-06 12:53:02 +0100

    Deployment

--- pages/Bayesian Calculator.py
+++ pages/Bayesian Calculator.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
 
+# ==============================
+#   PAGE CONFIG (WIDE MODE)
+# ==============================
+
+st.set_page_config(layout="wide")
+st.title("Bayesian calculator")
